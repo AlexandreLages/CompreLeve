cef90825e56912538057ce5e24af29f15141830a
Artur Miranda <ldm.artur@gmail.com>
2017-05-10 09:07:27 -0300

    Remove guia de enderecos e adiciona classes da tela de recibos

---
+++ CompraLeve/RecibosTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RecibosTableViewCell.swift
+//  CompraLeve
+//
+//  Created by Student on 5/10/17.
+//  Copyright © 2017 Wolves. All rights reserved.
+//
+
+import UIKit
+
+class RecibosTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
